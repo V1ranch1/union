1248921c016ef84d213229a847b6847925f35061
Vitaly Sobko <65503596+V1ranch1@users.noreply.github.com>
2022-06-19 20:48:45 +0300

    Добавили блок форму

---
+++ js/main.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('.formCalendar').forEach((item) => {
+    const today = new Date();
+    const input = item.querySelector('input');
+    input.value = today.toLocaleDateString('ru-RU');
+
+    const calendar = datepicker(item, {
+        customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        customMonths: ['Январь', 'Ферваль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        onSelect: (instance, date) => {
+            input.value = new Date(date).toLocaleDateString('ru-RU');
+        },
+    });
+});
+
+document.querySelectorAll('.formSelect').forEach((item) => {
+    const input = item.querySelector('.formInput');
+    const value = item.querySelectorAll('.formSelectValue');
+    const active = item.querySelector('.formSelectActive');
+    const hidden = item.querySelector('.formSelectHidden');
+    
+    input.addEventListener('click', () => {
+        item.classList.add('formSelect_active');
+    }, true);
+
+    value.forEach((valueItem) => {
+        valueItem.addEventListener('click', (e) => {
+            const newValue = e.target.innerHTML;
+            active.innerHTML = newValue;
+            hidden.value = newValue;
+            item.classList.remove('formSelect_active');
+        }, false);
+    });
+});
