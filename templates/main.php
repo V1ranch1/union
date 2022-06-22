9c5d5eaa2e0fd22dd2741c9452126f9f47e8a033
Mikhail Durmanov <madurmanov@gmail.com>
2022-06-22 21:14:39 +0300

    Remove google ads

--- templates/main.php
+++ templates/main.php
@@ -14,8 +14,6 @@
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/datepicker.min.css">
     <link rel="stylesheet" href="/css/main.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;900&display=swap" rel="stylesheet">
 </head>
 <body>
