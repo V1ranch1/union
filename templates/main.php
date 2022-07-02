e0b87e68729a911bcad1f0f8b3038d99e95820c2
Vitaly Sobko <65503596+V1ranch1@users.noreply.github.com>
2022-07-02 18:12:53 +0300

    Добавил Яндекс Метрику

--- templates/main.php
+++ templates/main.php
@@ -169,5 +169,20 @@
     <script src="/js/datepicker.min.js"></script>
     <script src="/js/mask.js"></script>
     <script src="/js/main.js"></script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(89390481, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+    });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/89390481" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </body>
 </html>
